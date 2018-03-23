3f600ec7776c42e066839fabb1ee2806da44c31e
MaeganW <maeganwomble@gmail.com>
2018-03-23 14:33:08 -0500

    handlebars register partials and register helpers

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Node-Web-Server
 Creating a custom API using Node.js and Express.
+
+Uses: Node.js, Express, Handlebars
--- server.js
+++ server.js
@@ -2,32 +2,37 @@ const express = require('express');
 const hbs = require('hbs');
 const app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (request, response) => {
-  // response.send('<h1>Hello World!</h1>');
-  response.send({
-    name: 'Maegan',
-    likes: [
-      'books',
-      'curry',
-      'coffee'
-    ]
-  });
-});
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+})
+
+// app.get('/', (request, response) => {
+//   // response.send('<h1>Hello World!</h1>');
+//   response.send({
+//     name: 'Maegan',
+//     likes: [
+//       'books',
+//       'curry',
+//       'coffee'
+//     ]
+//   });
+// });
 
 app.get('/about', (request, response) => {
   response.render('about.hbs', {
     title: 'About',
-    currentYear: new Date().getFullYear()
+    // currentYear: new Date().getFullYear()
   });
 });
 
-app.get('/home', (request, response) => {
+app.get('/', (request, response) => {
   response.render('home.hbs', {
     title: 'Home',
-    currentYear: new Date().getFullYear(),
+    // currentYear: new Date().getFullYear(),
     currentUser: 'Maegan'
   })
 })
